3adf7ef7a2b891aa75facb23874c05a2a7fadf2c
Aryan-986 <aryankarki984@gmail.com>
2024-11-21 21:58:46 +0545

    I have added the demo how section work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <main className="text-red-500 text-3xl font-bold">
-    Aryan
+   <>
+    <Header/>
+    <main>
+    <Outlet/>
   </main>
+  <Footer/>
+   </>
   )
 }
 
