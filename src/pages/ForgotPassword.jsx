ec52149bc9804fa2564d88676cada0ef1b3030cd
Aryan-986 <aryankarki984@gmail.com>
2024-11-23 22:41:00 +0545

    Forgot pass and otp verification with email send done with ui

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { FaRegEye } from "react-icons/fa6";
 import toast from 'react-hot-toast';
@@ -7,12 +7,10 @@ import SummaryApi from '../common/SummaryApi';
 import AxiosToastError from '../utils/AxiosToastError';
 import { Link, useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const ForgotPassword = () => {
     const [data, setData] = useState({
         email: "",
-        password: "",
     })
-    const [showPassword, setShowPassword] = useState(false)
     const navigate = useNavigate()
 
     const handleChange = (e) => {
@@ -34,7 +32,7 @@ const Login = () => {
 
         try {
             const response = await Axios({
-                ...SummaryApi.login,
+                ...SummaryApi.forgot_password,
                 data : data
             })
             
@@ -44,11 +42,13 @@ const Login = () => {
 
             if(response.data.success){
                 toast.success(response.data.message)
+                navigate("/verification-otp",{
+                  state : data
+                })
                 setData({
                     email : "",
-                    password : "",
                 })
-                navigate("/")
+                
             }
 
         } catch (error) {
@@ -58,15 +58,12 @@ const Login = () => {
 
 
     }
+
     return (
         <section className='w-full container mx-auto px-2'>
             <div className='bg-white my-4 w-full max-w-lg mx-auto rounded p-7'>
-                <div className="font-bold text-blue-900">
-                <p>Welcome to Go Dam</p>
-                </div>
-
+                <p className='font-semibold text-lg'>Forgot Password </p>
                 <form className='grid gap-4 py-4' onSubmit={handleSubmit}>
-                    
                     <div className='grid gap-1'>
                         <label htmlFor='email'>Email :</label>
                         <input
@@ -79,41 +76,18 @@ const Login = () => {
                             placeholder='Enter your email'
                         />
                     </div>
-                    <div className='grid gap-1'>
-                        <label htmlFor='password'>Password :</label>
-                        <div className='bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200'>
-                            <input
-                                type={showPassword ? "text" : "password"}
-                                id='password'
-                                className='w-full outline-none'
-                                name='password'
-                                value={data.password}
-                                onChange={handleChange}
-                                placeholder='Enter your password'
-                            />
-                            <div onClick={() => setShowPassword(preve => !preve)} className='cursor-pointer'>
-                                {
-                                    showPassword ? (
-                                        <FaRegEye />
-                                    ) : (
-                                        <FaRegEyeSlash />
-                                    )
-                                }
-                            </div>
-                        </div>
-                        <Link to={"/forgot-password"} className='block ml-auto hover:text-blue-700'>Forgot Password?</Link>
-                    </div>
-
-                    <button disabled={!valideValue} className={` ${valideValue ? "bg-green-800 hover:bg-green-700" : "bg-gray-500" }    text-white py-2 rounded font-semibold my-3 tracking-wide`}>LogIn</button>
+             
+                    <button disabled={!valideValue} className={` ${valideValue ? "bg-black hover:bg-gray-500" : "bg-gray-500" }    text-white py-2 rounded font-semibold my-3 tracking-wide`}>Send Otp</button>
 
                 </form>
 
                 <p>
-                    Do you have account? <Link to={"/register"} className='font-semibold text-blue-600 hover:text-blue-800'>Register</Link>
+                    Already have account? <Link to={"/login"} className='font-semibold text-blue-500 hover:text-blue-700'>Login</Link>
                 </p>
             </div>
         </section>
     )
 }
 
-export default Login
+export default ForgotPassword
+
